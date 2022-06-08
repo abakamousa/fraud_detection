c90c0fbca25c1cf7bf6fd122416d04784e0046eb
ABOUBAKAR Moussa <abakamousa@gmail.com>
2022-06-08 14:27:39 +0200

    V1.7

--- functions.py
+++ functions.py
@@ -2,10 +2,47 @@ import numpy             as np # linear algebra
 import pandas            as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import seaborn           as sns
 import matplotlib.pyplot as plt
+import random
 
 
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif
+from IPython.display           import HTML
+
+
+def hide_toggle(for_next=False):
+    this_cell = """$('div.cell.code_cell.rendered.selected')"""
+    next_cell = this_cell + '.next()'
+
+    toggle_text = 'Toggle show/hide'  # text shown on toggle link
+    target_cell = this_cell  # target cell to control with toggle
+    js_hide_current = ''  # bit of JS to permanently hide code in current cell (only when toggling next cell)
+
+    if for_next:
+        target_cell = next_cell
+        toggle_text += ' next cell'
+        js_hide_current = this_cell + '.find("div.input").hide();'
+
+    js_f_name = 'code_toggle_{}'.format(str(random.randint(1,2**64)))
+
+    html = """
+        <script>
+            function {f_name}() {{
+                {cell_selector}.find('div.input').toggle();
+            }}
+
+            {js_hide_current}
+        </script>
+
+        <a href="javascript:{f_name}()">{toggle_text}</a>
+    """.format(
+        f_name=js_f_name,
+        cell_selector=target_cell,
+        js_hide_current=js_hide_current, 
+        toggle_text=toggle_text
+    )
+
+    return HTML(html)
 
 def plot_nas(df: pd.DataFrame):
     if df.isnull().sum().sum() != 0:
